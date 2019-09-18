e0959157b5276cc97b63e0a8b5687076249a6cc6
Patrick Beart <patrick.beart@gmail.com>
2019-09-18 20:55:07 +0100

    Added freeze.bat

---
+++ freeze.bat
@@ -0,0 +1,3 @@
+"C:\Users\verya\AppData\Local\Programs\Python\Python37-32\scripts\pyinstaller.exe" --distpath "pyinstaller" --workpath "pyinstaller\work" --icon "src\icon.ico" --add-data "src\icon.png;." --noconfirm --windowed --name gacp src\start.py 
+
+pause
