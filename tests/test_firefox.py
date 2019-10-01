8b10ccb7f359febff6f15889e525bda9d97ee20e
Patrick Beart <patrick.beart@gmail.com>
2019-10-01 11:39:55 +0100

    Inital work on implementing summary generation tests

---
+++ tests/test_firefox.py
@@ -0,0 +1,24 @@
+"""
+Test Firefox 3+ specific parsing functionality
+"""
+
+import pytest
+
+import cookie_parser
+
+def test_summary():
+    cookies = ["_ga", "__utma", "__utmb", "__utmz"]
+    parser = cookie_parser.get_cookie_fetcher("firefox.3+",
+                                              "tests\\firefox.sqlite",
+                                              cookies)
+
+    summary = parser.get_domain_info(".testdomain.com")
+
+    # The values of the cookies in firefox.sqlite are such that
+    # get_domain_info should return:
+
+    expected_output = {"value_visitor_identifier": "2100671096",
+                       "time_most_recent_visit": }
+
+
+    print(summary)
